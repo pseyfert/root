0d28f7dc2e88f52e7378e45abbdfae4c407b73d2
Paul Seyfert <pseyfert@cern.ch>
2016-05-26 20:01:35 +0200

    copy&paste error in cmake comment
    rough draft for lzo
    
    complete ifdef guards
    
    lzo now might already work
    
    missing parenthesis in cmake file
    
    first compilation round
    
    fix cmake file
    
    mixed up C and C++ compilation
    
    reorder typedefs and function declarations
    
    catching another typo
    
    use root macro to extend cmake flags
    
    too stupidly copied and pasted
    
    more copy&paste errors fixed
    
    attack compiler warnings
    
    lz4 draft -- FindLZ4.cmake missing
    
    fail on incompatible options
    
    syntax error
    
    avoid type conversions and lzo types in lz4
    
    make compiler happy
    
    specify failure reason and exclude checksum test
    
    reorganise cmake stuff
    
    accidentially ignored target size
    
    really dont use builtin lz4 if system one is requested
    
    cmake corrections
    
    fiddle around with find lz4 module
    
    zopfli draft
    
    make zopfli build
    
    cut and paste zopfli down
    
    excessive printout
    
    draft brotli
    
    catch first compiler errors with brotli
    
    attacking more brotli errors
    
    after compiler errors come linker errors
    
    some tests
    
    expose lzo configuration to -D flags in cmake
    
    disable unused parameter warnings
    
    add header comments
    
    run astyle
    
    remove debugging output
    
    cleanup cmake
    
    compiler warnings and dependency cleanup
    
    compiler warnings
    
    remove lzo dependency from brotli by using adler32 from zlib
    
    add brotli documentation
    
    lz4 documentation
    
    added acknowledgments since it is mainly copy and paste
    
    copy and paste duplication
    
    Conflicts:
    
    	cmake/modules/RootBuildOptions.cmake

--- core/zip/src/Bits.h
+++ core/zip/src/Bits.h
@@ -15,6 +15,50 @@
 #include "zlib.h"
 #include "RConfigure.h"
 #include "ZipLZMA.h"
+#ifdef LZO
+extern void R__zipLZO(int cxlevel, int *srcsize, char *src, int *tgtsize, char *tgt, int *irep) ;
+#else
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wunused-parameter"
+void R__zipLZO(int cxlevel, int *srcsize, char *src, int *tgtsize, char *tgt, int *irep) {
+  R__error("ROOT built without LZO support");
+  return ;
+}
+#pragma GCC diagnostic pop
+#endif
+#ifdef LZ4
+extern void R__zipLZ4(int cxlevel, int *srcsize, char *src, int *tgtsize, char *tgt, int *irep) ;
+#else
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wunused-parameter"
+void R__zipLZ4(int cxlevel, int *srcsize, char *src, int *tgtsize, char *tgt, int *irep) {
+  R__error("ROOT built without LZ4 support");
+  return ;
+}
+#pragma GCC diagnostic pop
+#endif
+#ifdef ZOPFLI
+extern void R__zipZOPFLI(int cxlevel, int *srcsize, char *src, int *tgtsize, char *tgt, int *irep) ;
+#else
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wunused-parameter"
+void R__zipZOPFLI(int cxlevel, int *srcsize, char *src, int *tgtsize, char *tgt, int *irep) {
+  R__error("ROOT built without ZOPFLI support");
+  return ;
+}
+#pragma GCC diagnostic pop
+#endif
+#ifdef BROTLI
+extern void R__zipBROTLI(int cxlevel, int *srcsize, char *src, int *tgtsize, char *tgt, int *irep) ;
+#else
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wunused-parameter"
+void R__zipBROTLI(int cxlevel, int *srcsize, char *src, int *tgtsize, char *tgt, int *irep) {
+  R__error("ROOT built without BROTLI support");
+  return ;
+}
+#pragma GCC diagnostic pop
+#endif
 
 #include <stdio.h>
 #include <assert.h>
@@ -287,11 +331,17 @@ struct bits_internal_state {
    and when R__zipMultipleAlgorithm is called with its last argument set to 0.
    R__ZipMode = 1 : ZLIB compression algorithm is used (default)
    R__ZipMode = 2 : LZMA compression algorithm is used
+   R__ZipMode = 4 : LZO compression algorithm is used
+   R__ZipMode = 5 : LZ4 compression algorithm is used
+   R__ZipMode = 6 : ZOPFLI compression algorithm is used
+   R__ZipMode = 7 : BROTLI compression algorithm is used
    R__ZipMode = 0 or 3 : a very old compression algorithm is used
    (the very old algorithm is supported for backward compatibility)
    The LZMA algorithm requires the external XZ package be installed when linking
    is done. LZMA typically has significantly higher compression factors, but takes
    more CPU time and memory resources while compressing.
+
+TODO: add comments for lzo,lz4,zopfli,brotli
 */
 int R__ZipMode = 1;
 
@@ -566,6 +616,10 @@ void R__zipMultipleAlgorithm(int cxlevel, int *srcsize, char *src, int *tgtsize,
      /*                      1 = zlib */
      /*                      2 = lzma */
      /*                      3 = old */
+     /*                      4 = lzo */
+     /*                      5 = lz4 */
+     /*                      6 = zopfli (zlib) */
+     /*                      7 = brotli */
 {
   int err;
   int method   = Z_DEFLATED;
@@ -579,9 +633,27 @@ void R__zipMultipleAlgorithm(int cxlevel, int *srcsize, char *src, int *tgtsize,
     compressionAlgorithm = R__ZipMode;
   }
 
+  R__error("calling some compression");
   // The LZMA compression algorithm from the XZ package
   if (compressionAlgorithm == 2) {
     R__zipLZMA(cxlevel, srcsize, src, tgtsize, tgt, irep);
+    return;
+  } else if (compressionAlgorithm == 4) {
+    // The LZO compression algorithm
+    R__zipLZO(cxlevel, srcsize, src, tgtsize, tgt, irep);
+    return;
+  } else if (compressionAlgorithm == 5) {
+    // The LZ4 compression algorithm
+    R__zipLZ4(cxlevel, srcsize, src, tgtsize, tgt, irep);
+    return;
+  } else if (compressionAlgorithm == 5) {
+    // The ZOPFLI compression algorithm
+    R__error("calling zopfli");
+    R__zipZOPFLI(cxlevel, srcsize, src, tgtsize, tgt, irep);
+    return;
+  } else if (compressionAlgorithm == 6) {
+    // The BROTLI compression algorithm
+    R__zipBROTLI(cxlevel, srcsize, src, tgtsize, tgt, irep);
     return;
   }
 
